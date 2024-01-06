9be690c985a8e02d8095866d0479051864f48747
weremite <weremite@gmail.com>
2024-01-06 19:08:05 +0200

    Add better solution to split-string task

--- CodeWars/6kyu/Split Strings/split-strings.php
+++ CodeWars/6kyu/Split Strings/split-strings.php
@@ -10,28 +10,42 @@ Examples:
 * 'abcdef' => ['ab', 'cd', 'ef']
 */
 
+// first variation
+// function solution($str) {
+//     $tmp = $str;
+//     $arr = array();
+
+//     //todo #1 count string length    
+//     $len = strlen($tmp);
+
+//     //todo #2 check if length odd or even, add underscore to odd number
+//     if ($len == 0) {
+//         return $arr;
+//     } elseif ($len % 2 != 0) {
+//         $tmp .= "_";
+//     }
+
+//     //todo #3 split string to array
+//     for ($i = 0; $i < $len; $i+=2) {
+//        $arr[] = substr($tmp, $i, 2);
+//     }
+
+//     return $arr;
+// }
+
 function solution($str) {
-    $tmp = $str;
-    $arr = array();
-
-    //todo #1 count string length    
-    $len = strlen($tmp);
-
-    //todo #2 check if length odd or even, add underscore to odd number
-    if ($len == 0) {
-        return $arr;
-    } elseif ($len % 2 != 0) {
-        $tmp .= "_";
+    if ($str == "") {
+        return [];
     }
 
-    //todo #3 split string to array
-    for ($i = 0; $i < $len; $i+=2) {
-       $arr[] = substr($tmp, $i, 2);
+    if (strlen($str) % 2 != 0) {
+        $str .= "_";
     }
 
-    return $arr;
+    return str_split($str, 2);
 }
 
 print_r(solution("abc"));
 print_r(solution("abcdef"));
+print_r(solution(""));
 ?>
